d86fcacafa9aa880e347664169debc56a7408f1e
Alessio <alessio.velluso@gmail.com>
2025-05-30 16:26:59 +0200

    :bookmark: v0.3.8-dev

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,4 +1,6 @@
+import { BackgroundService } from "./BackgroundService";
 import { RouterWrapper } from "./RouterWrapper";
+import { SocketWrapper } from "./SocketWrapper";
 import { Starter } from "./Starter";
 import { Swagger } from "./Swagger";
 import { Token } from "./Token";
@@ -6,10 +8,11 @@ import { Token } from "./Token";
 export * from "../types/db.sptypes";
 export * from "../types/generic.sptypes";
 export * from "../types/router.sptypes";
+export * from "../types/socket.sptypes";
 export * from "../types/starter.sptypes";
 export * from "../types/swagger.sptypes";
 export * from "../types/token.sptypes";
 export * from "./_utils";
 
 
-export { RouterWrapper, Starter, Swagger, Token };
+export { BackgroundService, RouterWrapper, SocketWrapper, Starter, Swagger, Token };
